01179637c5af0335fcbeeaba28b7bfd1d6f39312
Jesus Martinez <74986138+jm-jesusm@users.noreply.github.com>
2024-02-02 17:54:25 +0100

    Fill my description

--- team.js
+++ team.js
@@ -1,9 +1,25 @@
-const estudiantes = [{name:"Jairo", 
-age: 26, 
-city: "Granada", 
-hobby:"games", 
-favoriteFood: "Spicy",
-favoriteVideoGame: "Legend of Zelda",
-favoriteFilm: "Disney",
-favoriteBook:"Invierno en Paris",
-petName:"Lasobona"}]
+const estudiantes = [
+    {
+        name:"Jairo", 
+        age: 26, 
+        city: "Granada", 
+        hobby:"games", 
+        favoriteFood: "Spicy",
+        favoriteVideoGame: "Legend of Zelda",
+        favoriteFilm: "Disney",
+        favoriteBook:"Invierno en Paris",
+        petName:"Lasobona"
+    },
+    {
+        name: "Jesus",
+        surname: "Martinez Garcia",
+        age: 20,
+        city: "Murcia",
+        hobby: "Video-games",
+        favouriteFood: "Spaghetties",
+        favouriteVideoGame: "Escape from Tarkov",
+        favouriteFilm: "Tron",
+        favouriteBook: "Las lagrimas de Shiva",
+        petName: "Spay"
+    }
+]
