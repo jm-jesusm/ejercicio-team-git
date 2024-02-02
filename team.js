812ca013caa5848913d61cd65b3da4f67cd41f4c
unknown <jairo.rom.garzon@gmail.com>
2024-02-02 17:46:10 +0100

    cambios Jairo

--- team.js
+++ team.js
@@ -1,1 +1,9 @@
-const estudiantes = []
+const estudiantes = [{name:"Jairo", 
+age: 26, 
+city: "Granada", 
+hobby:"games", 
+favoriteFood: "Spicy",
+favoriteVideoGame: "Legend of Zelda",
+favoriteFilm: "Disney",
+favoriteBook:"Invierno en Paris",
+petName:"Lasobona"}]
