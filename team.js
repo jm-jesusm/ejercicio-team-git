4f1493197e34572f5e02f4f350d2b678d86bd1f3
Jesus Martinez <74986138+jm-jesusm@users.noreply.github.com>
2024-02-02 17:54:25 +0100

    Fill my description

--- team.js
+++ team.js
@@ -1,1 +1,12 @@
-const estudiantes = []
+const estudiantes = [{
+    name: "Jesus",
+    surname: "Martinez Garcia",
+    age: 20,
+    city: "Murcia",
+    hobby: "Video-games",
+    favouriteFood: "Spaghetties",
+    favouriteVideoGame: "Escape from Tarkov",
+    favouriteFilm: "Tron",
+    favouriteBook: "Las lagrimas de Shiva",
+    petName: "Spay"
+}]
